90e9a177e7a3cb398d5eb876f56fc62a7a0cb78b
Alexey Medov <amedov@gitlab.int>
2021-11-18 00:36:09 +0300

    add new configs

---
+++ how_to_start/docker_container/v3/fluent-bit_version_1.8.9.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+#------------------------
+
+app=fluent-bit
+version=1.8.9
+
+#------------------------
+
+if [ "$1" == "delete" ] || [ "$1" == "drop" ] || [ "$1" == "uninstall" ] || [ "$1" == "remove" ]; then
+   docker rm -f $app > /dev/null 2>&1
+   docker rmi fluent/fluent-bit:$version > /dev/null 2>&1
+   echo ""
+   echo "Fluent-bit was removed"
+   echo ""
+   exit 0
+fi
+
+#------------------------
+
+docker run -d --restart always --name $app \
+-v $(pwd)/configs/fluent-bit.conf:/fluent-bit/etc/fluent-bit.conf \
+-v $(pwd)/configs/parsers.conf:/fluent-bit/etc/parsers.conf \
+-v /var/lib/docker/containers:/var/lib/docker/containers \
+-p 24224:24224 \
+--network=demonstration \
+fluent/$app:$version || exit 1
+
+#------------------------
+
+sleep 5
+
+docker ps -a | grep $app
+
+if [ "$(docker ps -a | grep $app | grep xit > /dev/null 2>&1; echo $?)" -eq "0" ]; then docker logs -n 30 $app; docker rm -f $app; exit 1; fi
+
+docker logs $app
+
+echo ""
+echo "=================================="
+echo "Fluent-bit started successfully =)"
+echo "=================================="
+echo ""
+
+docker cp /bin/cat $app:/bin/cat > /dev/null 2>&1
+docker cp /bin/ls $app:/bin/ls > /dev/null 2>&1
+docker cp /bin/echo $app:/bin/echo > /dev/null 2>&1
+docker cp /bin/pwd $app:/bin/pwd > /dev/null 2>&1
+
+echo "With this configuration file: /fluent-bit/etc/fluent-bit.conf"
+echo ""
+docker exec -it $app cat /fluent-bit/etc/fluent-bit.conf
+echo ""
